5bae7c26f2d6c75d5b282d1d1a5bf476e3e1626c
Nelson Kamau <nelson.kaguamere@strathmore.edu>
2023-10-17 11:41:32 +0300

    created submission file in .R

---
+++ Lab4-Submission-ExposingTheStructureOfDataUsingDataTransforms.R
@@ -0,0 +1,59 @@
+# STEP 1. Install and Load the Required Packages ----
+## mlbench ----
+if (require("mlbench")) {
+  require("mlbench")
+} else {
+  install.packages("mlbench", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## readr ----
+if (require("readr")) {
+  require("readr")
+} else {
+  install.packages("readr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## caret ----
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## e1071 ----
+if (require("e1071")) {
+  require("e1071")
+} else {
+  install.packages("e1071", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## factoextra ----
+if (require("factoextra")) {
+  require("factoextra")
+} else {
+  install.packages("factoextra", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## FactoMineR ----
+if (require("FactoMineR")) {
+  require("FactoMineR")
+} else {
+  install.packages("FactoMineR", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## STEP 2. Load the Datasets ----
+### StudentPerformance Dataset ----
+# Execute the following to load the downloaded Crop dataset:
+student_performance_dataset <- read_csv("data/StudentPerformanceDataset.csv",
+  col_types = cols(
+    density = col_factor(levels = c("1", "2")),
+    block = col_factor(levels = c("1", "2", "3", "4")),
+    fertilizer = col_factor(levels = c("1", "2", "3"))
+  )
+)
